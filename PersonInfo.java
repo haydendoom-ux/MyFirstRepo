d464a5440ea59fe44be796d754b0e318749194e6
Hayden <haydendoom@csuglobal.edu>
2026-03-17 12:54:26 -0600

    Initial commit

---
+++ PersonInfo.java
@@ -0,0 +1,15 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "123 Maple Street";
+        String city = "Denver";
+        String zipCode = "80201";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
